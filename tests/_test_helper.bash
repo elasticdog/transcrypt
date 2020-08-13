86b1f3098802cdec59c7d2bdbe186c5b42a282d8
James Murty <james@murty.co>
2020-08-14 09:25:13 +1000

    Always use double-brackets in tests, and comply with shellcheck rules

--- tests/_test_helper.bash
+++ tests/_test_helper.bash
@@ -1,46 +1,46 @@
 function init_git_repo {
   # Warn and do nothing if test dir envvar is unset
-  if [ -z $BATS_TEST_DIRNAME ]; then
+  if [[ -z "$BATS_TEST_DIRNAME" ]]; then
     echo "WARNING: Required envvar \$BATS_TEST_DIRNAME is unset"
   # Warn and do nothing if test git repo path already exists
-  elif [ -e $BATS_TEST_DIRNAME/.git ]; then
+  elif [[ -e "$BATS_TEST_DIRNAME/.git" ]]; then
     echo "WARNING: Test repo already exists at $BATS_TEST_DIRNAME/.git"
   else
     # Initialise test git repo at the same path as the test files
-    git init $BATS_TEST_DIRNAME
+    git init "$BATS_TEST_DIRNAME"
     # Flag test git repo as 100% the test one, for safety before later removal
-    touch $BATS_TEST_DIRNAME/.git/repo-for-transcrypt-bats-tests
+    touch "$BATS_TEST_DIRNAME"/.git/repo-for-transcrypt-bats-tests
   fi
 }
 
 function nuke_git_repo {
   # Warn and do nothing if test dir envvar is unset
-  if [ -z $BATS_TEST_DIRNAME ]; then
+  if [[ -z "$BATS_TEST_DIRNAME" ]]; then
     echo "WARNING: Required envvar \$BATS_TEST_DIRNAME is unset"
   # Warn and do nothing if the test git repo is missing the flag file that
   # ensures it *really* is the test one, as set by the 'init_git_repo' function
-  elif [ ! -e $BATS_TEST_DIRNAME/.git/repo-for-transcrypt-bats-tests ]; then
+  elif [[ ! -e "$BATS_TEST_DIRNAME/.git/repo-for-transcrypt-bats-tests" ]]; then
     echo "WARNING: Aborting delete of non-test Git repo at $BATS_TEST_DIRNAME/.git"
   else
     # Forcibly delete the test git repo
-    rm -fR $BATS_TEST_DIRNAME/.git
+    rm -fR "$BATS_TEST_DIRNAME"/.git
   fi
 }
 
 function cleanup_all {
   nuke_git_repo
-  rm $BATS_TEST_DIRNAME/.gitattributes
-  rm -f $BATS_TEST_DIRNAME/sensitive_file
+  rm "$BATS_TEST_DIRNAME"/.gitattributes
+  rm -f "$BATS_TEST_DIRNAME"/sensitive_file
 }
 
 function init_transcrypt {
-  $BATS_TEST_DIRNAME/../transcrypt --cipher=aes-256-cbc --password=abc123 --yes
+  "$BATS_TEST_DIRNAME"/../transcrypt --cipher=aes-256-cbc --password=abc123 --yes
 }
 
 function encrypt_named_file {
   filename="$1"
   content=$2
-  if [ "$content" ]; then
+  if [[ "$content" ]]; then
     echo "$content" > "$filename"
   fi
   echo "\"$filename\" filter=crypt diff=crypt merge=crypt" >> .gitattributes
@@ -49,14 +49,14 @@ function encrypt_named_file {
 }
 
 function setup {
-  pushd $BATS_TEST_DIRNAME
+  pushd "$BATS_TEST_DIRNAME" || exit 1
   init_git_repo
-  if [ ! "$SETUP_SKIP_INIT_TRANSCRYPT" ]; then
+  if [[ ! "$SETUP_SKIP_INIT_TRANSCRYPT" ]]; then
     init_transcrypt
   fi
 }
 
 function teardown {
   cleanup_all
-  popd
+  popd || exit 1
 }
--- tests/test_merge.bats
+++ tests/test_merge.bats
@@ -1,6 +1,6 @@
 #!/usr/bin/env bats
 
-load $BATS_TEST_DIRNAME/_test_helper.bash
+load "$BATS_TEST_DIRNAME/_test_helper.bash"
 
 @test "merge: branches with encrypted file - addition, no conflict" {
   echo "1. First step" > sensitive_file
@@ -15,9 +15,9 @@ load $BATS_TEST_DIRNAME/_test_helper.bash
   git merge branch-2
 
   run cat sensitive_file
-  [ "$status" -eq 0 ]
-  [ "${lines[0]}" = "1. First step" ]
-  [ "${lines[1]}" = "2. Second step" ]
+  [[ "$status" -eq 0 ]]
+  [[ "${lines[0]}" = "1. First step" ]]
+  [[ "${lines[1]}" = "2. Second step" ]]
 }
 
 @test "merge: branches with encrypted file - line change incoming branch, no conflict" {
@@ -32,12 +32,12 @@ load $BATS_TEST_DIRNAME/_test_helper.bash
 
   git checkout -
   run git merge branch-2
-  [ "$status" -eq 0 ]
+  [[ "$status" -eq 0 ]]
 
   run cat sensitive_file
-  [ "$status" -eq 0 ]
-  [ "${lines[0]}" = "1. Step the first" ]
-  [ "${lines[1]}" = "2. Second step" ]
+  [[ "$status" -eq 0 ]]
+  [[ "${lines[0]}" = "1. Step the first" ]]
+  [[ "${lines[1]}" = "2. Second step" ]]
 }
 
 @test "merge: branches with encrypted file - line changes both branches, no conflict" {
@@ -60,13 +60,13 @@ load $BATS_TEST_DIRNAME/_test_helper.bash
   git commit -m "Add line 3"
 
   run git merge branch-2
-  [ "$status" -eq 0 ]
+  [[ "$status" -eq 0 ]]
 
   run cat sensitive_file
-  [ "$status" -eq 0 ]
-  [ "${lines[0]}" = "1. Step the first" ]
-  [ "${lines[1]}" = "2. Second step" ]
-  [ "${lines[2]}" = "3. Third step" ]
+  [[ "$status" -eq 0 ]]
+  [[ "${lines[0]}" = "1. Step the first" ]]
+  [[ "${lines[1]}" = "2. Second step" ]]
+  [[ "${lines[2]}" = "3. Third step" ]]
 }
 
 @test "merge: branches with encrypted file - line changes both branches, with conflicts" {
@@ -85,15 +85,15 @@ load $BATS_TEST_DIRNAME/_test_helper.bash
   git commit -m "Change line 1 in original branch to set up conflict"
 
   run git merge branch-2
-  [ "$status" -ne 0 ]
+  [[ "$status" -ne 0 ]]
   [[ "${output}" = *"CONFLICT (content): Merge conflict in sensitive_file"* ]]
 
   run cat sensitive_file
-  [ "$status" -eq 0 ]
-  [ "${lines[0]}" = "<<<<<<< master" ]
-  [ "${lines[1]}" = "a. First step" ]
-  [ "${lines[2]}" = "=======" ]
-  [ "${lines[3]}" = "1. Step the first" ]
-  [ "${lines[4]}" = "2. Second step" ]
-  [ "${lines[5]}" = ">>>>>>> branch-2" ]
+  [[ "$status" -eq 0 ]]
+  [[ "${lines[0]}" = "<<<<<<< master" ]]
+  [[ "${lines[1]}" = "a. First step" ]]
+  [[ "${lines[2]}" = "=======" ]]
+  [[ "${lines[3]}" = "1. Step the first" ]]
+  [[ "${lines[4]}" = "2. Second step" ]]
+  [[ "${lines[5]}" = ">>>>>>> branch-2" ]]
 }
