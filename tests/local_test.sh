f39759b8e5ffe96dbc6a6e0487acdba71bfb4852
James Murty <james@murty.co>
2022-07-09 23:59:39 +1000

    Remove `-pbkdf2` shorthand argument for `-k pbkdf2`
    We don't have algorithm-specific shorthands for other things like
    cipher, and if transcrypt will support multiple potential KDFs it
    will be unwieldy to add arguments for every one.

--- tests/local_test.sh
+++ tests/local_test.sh
@@ -1,5 +1,5 @@
 #./transcrypt -F -c aes-256-cbc -p "foobar" -md SHA512 -sm configured --use_pbkdf2=0
-./transcrypt -F -c aes-256-cbc -pbkdf2 -p "foobar" -md SHA512 -sm configured 
+./transcrypt -F -c aes-256-cbc -k pbkdf2 -p "foobar" -md SHA512 -sm configured
 
 ./transcrypt -d
 
